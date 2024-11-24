e3080668c063d499aeef4f07b99d045bf7cd0993
nitzano707 <nitzan.orot@gmail.com>
2024-11-24 13:50:00 +0200

    Update script.js

--- script.js
+++ script.js
@@ -393,7 +393,8 @@ function bufferToWaveBlob(abuffer) {
 async function processAudioChunk(chunk, transcriptionData, currentChunk, totalChunks) {
     const formData = new FormData();
     formData.append('file', chunk);
-    formData.append('model', 'whisper-large-v3-turbo');
+    //formData.append('model', 'whisper-large-v3-turbo');
+    formData.append('model', 'whisper-large-v3');
     formData.append('response_format', 'verbose_json'); 
     formData.append('language', defaultLanguage);
 
